3f492bbd5faf60fd162d17a5f170dc56719e0ab3
Victoria <victoriamoles@gmail.com>
2019-01-03 13:38:21 +0000

    Interface

---
+++ WindowsFormsApp1/WindowsFormsApp1/IDraw.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    interface IDraw
+    {
+        int getX();
+
+        int getY();
+
+        void moveTo(int x, int y);
+
+        void drawLine(int x, int y);
+
+        void drawRectangle(int width, int height);
+
+        void drawCircle(int radius);
+
+        void drawTriangle(int b, int height);
+
+        void drawPolygon(Point[] p);
+    }
+}
